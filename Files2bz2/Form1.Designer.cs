eb6f926e8ecda65f6eeea5e8fbc14416b444070b
Mell Rosandich <mell@ourace.com>
2014-12-30 19:10:26 -0600

    First Build
    First Commit

---
+++ Files2bz2/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace Files2bz2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1. Select a directory to copy from";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(57, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Click to select directory";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(177, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(587, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(203, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "2. Do you wish to include sub directories?";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(57, 114);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(177, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Check to include sub directories";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(401, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "3. Select the file types you want to bz2 zip. Click the button to list all the fi" +
+    "les types . ";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(57, 216);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(369, 121);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.Leave += new System.EventHandler(this.listBox1_Leave);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(57, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(188, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Click to populate the below list";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(54, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(537, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "This list will be generated from the the first two steps. It will read from the d" +
+    "irectory and sub directories (if selected)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(155, 340);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(194, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Use [ctrl] key to select multiple file types";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(109, 353);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(286, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "If you choose bz2 files they will be copied to the destination";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(40, 393);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(192, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "4. Choose a destination for the bz2 files";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(177, 418);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(587, 20);
+            this.textBox2.TabIndex = 13;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(57, 416);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(114, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Click to select directory";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(40, 466);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(169, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "5. BZ2 Zip the files and copy them";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(54, 482);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(175, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Start: BZ Zip and Copy the Files";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(54, 531);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(707, 147);
+            this.listBox2.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(651, 681);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(61, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Total Files: ";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(54, 508);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(136, 17);
+            this.checkBox2.TabIndex = 18;
+            this.checkBox2.Text = "Overwrite Exsisting files";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(177, 16);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(584, 20);
+            this.textBox3.TabIndex = 19;
+            this.textBox3.Text = "C:\\Program Files\\7-Zip\\7z.exe";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(54, 19);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(100, 13);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Path to 7zip. 7z.exe";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(51, 685);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(129, 13);
+            this.linkLabel1.TabIndex = 21;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Visit Ourace.com Website";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 707);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Files 2 bz2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Files2bz2/Form1.cs
@@ -0,0 +1,358 @@
+﻿/* Created by Mell Rosandich
+ * mell@ourace.com
+ * 
+ * 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace Files2bz2
+{
+    public partial class Form1 : Form
+    {
+        string g_filePathFrom = "";     // This is the path that will be read from
+        string g_filePathTo = "";       // This is the path the files will be copied to.
+        bool g_isprocessing = false;    // The state of processing files
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+                g_filePathFrom = folderBrowserDialog1.SelectedPath;
+                listBox1.Items.Clear();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (!Directory.Exists(g_filePathFrom))
+            {
+                MessageBox.Show("You need to set the directory in step 1");
+                return;
+            }
+
+            List<string> Files;
+            if (checkBox1.Checked)
+            {
+                Files = new List<string>(Directory.EnumerateFiles(g_filePathFrom, "*.*", SearchOption.AllDirectories));
+            }
+            else
+            {
+                Files = new List<string>(Directory.EnumerateFiles(g_filePathFrom, "*.*", SearchOption.TopDirectoryOnly));
+            }
+            
+            foreach (var File in Files)
+            {
+                Application.DoEvents();                     //let the application breath. Allow stopping.
+                var temp_Parts = File.Split('.');           //split the file string by a .
+                string fileExtension = temp_Parts.Last();   //get the very last element of the array. it will be the file type
+                if (!listBox1.Items.Contains(fileExtension))
+                {
+                    listBox1.Items.Add(fileExtension);
+                }
+                
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox2.Text = folderBrowserDialog1.SelectedPath;
+                g_filePathTo = folderBrowserDialog1.SelectedPath;
+                
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if( !File.Exists(textBox3.Text ) )
+            {
+                MessageBox.Show("Please enter the full path to the 7z.exe.");
+                return;
+            }
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("Please make sure you have set both folders in step 1 and 4.");
+                return;
+            }
+            if (!Directory.Exists(textBox1.Text))
+            {
+                MessageBox.Show("The directory path in step 1 is wrong.");
+                return;
+            }
+            if (!Directory.Exists(textBox2.Text))
+            {
+                MessageBox.Show("The directory path in step 4 is wrong.");
+                return;
+            }
+
+            int SelectedItems = 0;
+            for (int x = 0; x < listBox1.Items.Count; x++)
+            {
+                if (listBox1.GetSelected(x) == true && listBox1.Items[x].ToString() !="")
+                {
+                    SelectedItems++;
+                }
+
+            }
+
+            if (SelectedItems == 0)
+            {
+                MessageBox.Show("You need to select the file type that you want to bz2 in step 3.");
+                return;
+            }
+
+
+            if (g_isprocessing == false)
+            {
+                g_isprocessing = true;
+                DisableEnableAll(false);
+                button4.Text = "Stop: BZ Zip and Copy the Files";
+                BZCopyFiles();
+                
+            }
+            else
+            {
+                button4.Text = "Start: BZ Zip and Copy the Files";
+                DisableEnableAll(true);
+                g_isprocessing = false;
+            }
+            
+        }
+
+        private void DisableEnableAll(bool isEnabled)
+        {
+
+            button1.Enabled = isEnabled;
+            button2.Enabled = isEnabled;
+            checkBox1.Enabled = isEnabled;
+            checkBox2.Enabled = isEnabled;
+            textBox1.Enabled = isEnabled;
+            textBox2.Enabled = isEnabled;
+            listBox1.Enabled = isEnabled;
+            listBox2.Enabled = isEnabled;
+
+        }
+
+        public void CountFilesToMove()
+        {
+            List<string> Files;
+
+            if (checkBox1.Checked)
+            {
+                Files = new List<string>(Directory.EnumerateFiles(g_filePathFrom, "*.*", SearchOption.AllDirectories));
+            }
+            else
+            {
+                Files = new List<string>(Directory.EnumerateFiles(g_filePathFrom, "*.*", SearchOption.TopDirectoryOnly));
+            }
+
+            int FilesToMoveCnt = 0;
+            foreach (var TheFile in Files)
+            {
+                var temp_Parts = TheFile.Split('.');           //split the file string by a .
+                string fileExtension = temp_Parts.Last();   //get the very last element of the array. it will be the file type
+                if (isSelectedInList(fileExtension))
+                {
+                    FilesToMoveCnt++;
+                }
+                label9.Text = "Total Files: 0 of " + FilesToMoveCnt;
+            }
+
+        }
+
+
+        public void BZCopyFiles()
+        {
+            List<string> Files;
+            int FileCount = 0;
+            
+
+            if (checkBox1.Checked)
+            {
+                Files = new List<string>(Directory.EnumerateFiles(g_filePathFrom, "*.*", SearchOption.AllDirectories));
+            }
+            else
+            {
+                Files = new List<string>(Directory.EnumerateFiles(g_filePathFrom, "*.*", SearchOption.TopDirectoryOnly));
+            }
+
+
+            //count all the files usin
+            int FilesToMoveCnt = 0;
+            foreach (var TheFile in Files)
+            {
+                var temp_Parts = TheFile.Split('.');           //split the file string by a .
+                string fileExtension = temp_Parts.Last();   //get the very last element of the array. it will be the file type
+                if (isSelectedInList(fileExtension))
+                {
+                    FilesToMoveCnt++;
+                }
+                label9.Text = "Total Files: 0 of " + FilesToMoveCnt;
+            }
+
+            foreach (var TheFile in Files)
+            {
+                Application.DoEvents();                     //let the application breath. Allow stopping.
+                var temp_Parts = TheFile.Split('.');           //split the file string by a .
+                string fileExtension = temp_Parts.Last();   //get the very last element of the array. it will be the file type
+                
+                //exit if the button is pressed
+                if (g_isprocessing == false)
+                {
+                    return;
+                }
+
+                if (isSelectedInList(fileExtension) )
+                {
+                    listBox2.Items.Add("FROM: " + TheFile);
+                    FileCount++;
+                    label9.Text = "Total Files: " + FileCount + " of " + FilesToMoveCnt;
+
+                    //This will chop off the 
+                    string temp_Path = TheFile.Replace(g_filePathFrom, "");
+                    string new_Path = g_filePathTo + temp_Path;
+                    
+
+                    //Build the directory path
+                    //We will assume the to path is already built.
+                    //We need to break up the temp path into parts and check each part except the file name
+                    var Tempparts = temp_Path.Split('\\');
+                    string checkpath = "";
+                    int PartCnt = Tempparts.Count() - 1; //remove the last item from the list becuase its the file name
+                    for (int ix = 0; ix < PartCnt; ix++)
+                    {
+                        checkpath = checkpath + Tempparts[ix];
+                        if (!System.IO.Directory.Exists(g_filePathTo + checkpath ))
+                        {
+                            System.IO.Directory.CreateDirectory(g_filePathTo + checkpath);
+                            //listBox2.Items.Add("CKPath: " + g_filePathTo + checkpath);
+                        }
+                        checkpath = checkpath + "\\";
+                    }
+
+
+                    if (fileExtension.ToLower() == "bz2") // we are just going to copy the file
+                    {
+                        if (File.Exists(new_Path) && checkBox2.Checked)
+                        {
+                            File.Copy(TheFile, new_Path, true);
+                            listBox2.Items.Add("TO Overwrite: " + new_Path);
+                        }
+                        else
+                        {
+                            if (!File.Exists(new_Path))
+                            {
+                                File.Copy(TheFile, new_Path, false);
+                                listBox2.Items.Add("TO New: " + new_Path);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (File.Exists(new_Path) && checkBox2.Checked)
+                        {
+                            File.Delete(new_Path);
+                            //CopyAndArchive(new_Path, TheFile);
+                            CopyAndArchive(new_Path + ".bz2", TheFile);
+                            listBox2.Items.Add("TO Overwrite: " + new_Path + ".bz2");
+                        }
+                        else
+                        {
+                            if (!File.Exists(new_Path))
+                            {
+                                //CopyAndArchive(new_Path, TheFile);
+                                CopyAndArchive(new_Path + ".bz2", TheFile);
+                                listBox2.Items.Add("TO New: " + new_Path + ".bz2");
+                            }
+                        }
+                    }
+
+                }//end if file extension
+            }//end for each files
+        }//End BZCopyFiles
+
+
+        private void CopyAndArchive(string DestinPath, string FileToBzip)
+        {
+            //C:\Program Files\7-Zip\7z.exe" u -tbzip2 %~nx1.bz2 %1
+            
+            ProcessStartInfo bzip2 = new ProcessStartInfo(textBox3.Text);
+            bzip2.Arguments = string.Format("u -tbzip2 \"{0}\" \"{1}\"", DestinPath, FileToBzip);
+            bzip2.RedirectStandardInput = true;
+            bzip2.UseShellExecute = false;
+            bzip2.CreateNoWindow = true;
+            bzip2.WindowStyle = ProcessWindowStyle.Hidden;
+            Process bzip2process = Process.Start(bzip2);
+            bzip2process.WaitForExit();
+
+        }
+
+
+
+
+
+       
+
+
+        public bool isSelectedInList(string InVal)
+        {
+            bool temp_RetVal = false;
+            for (int x = 0; x < listBox1.Items.Count; x++)
+            {
+                if (listBox1.GetSelected(x) == true && listBox1.Items[x].ToString() == InVal)
+                {
+                    return true;
+                }
+                   
+            }
+            return temp_RetVal;
+        }//end isSelectedInList
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CountFilesToMove();
+        }
+
+        private void listBox1_Leave(object sender, EventArgs e)
+        {
+            CountFilesToMove();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.ourace.com/135-files2bz2");
+        }
+
+
+    }//end class
+}//end name space
